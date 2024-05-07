a7437ec8a08a1514f81c5b1b30a85fed2db42b3d
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-05-07 09:36:08 -0400

    Initial Commit

---
+++ GroupGoalTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroupGoalTracker
+//
+//  Created by Rudraksh Monga on 2024-05-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroupGoalTracker/GroupGoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupGoalTrackerApp.swift
+//  GroupGoalTracker
+//
+//  Created by Rudraksh Monga on 2024-05-07.
+//
+
+import SwiftUI
+
+@main
+struct GroupGoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
